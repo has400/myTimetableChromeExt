da4dd5092a522ecf437471fe3eb7b21a4df76831
has400 <has400@live.com.au>
2018-03-11 23:16:50 +1100

    Chrome Ext Source

---
+++ RMIT.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	var labels = document.getElementsByClassName('primary-nav cf large-12 columns');
+
+	var list = labels[0].innerHTML.split('</ul>');
+	list = list[0] + '<li><a href="https://mytimetable.rmit.edu.au/even/student">myTimetable</a></li></ul>'
+
+	labels[0].innerHTML = list;
+});
